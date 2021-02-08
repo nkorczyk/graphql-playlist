a650cbd5e7ea90589b00122c8cdf8a2470acd555
Norbert Korczyk <norbert_korczyk@epam.com>
2021-02-08 10:11:01 +0100

    Connect to MongoDB & mutations

--- server/app.js
+++ server/app.js
@@ -4,6 +4,15 @@ const schema = require("./schema/schema");
 
 const app = express();
 
+const mongoose = require("mongoose");
+mongoose
+  .connect(
+    "mongodb+srv://norbert:test123@gql.d92kr.mongodb.net/gql?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("Connected to MongoDB Atlas"))
+  .catch((err) => console.log("Error: ", err.message));
+
 // bind express with graphql
 app.use(
   "/graphql",
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,5 +1,7 @@
 const graphql = require("graphql");
 const _ = require("lodash");
+const Book = require("../models/book");
+const Author = require("../models/Author");
 
 const {
   GraphQLObjectType,
@@ -90,6 +92,27 @@ const RootQuery = new GraphQLObjectType({
   },
 });
 
+const Mutation = new GraphQLObjectType({
+  name: "Mutation",
+  fields: {
+    addAuthor: {
+      type: AuthorType,
+      args: {
+        name: { type: GraphQLString },
+        age: { type: GraphQLInt },
+      },
+      resolve(parent, args) {
+        let author = new Author({
+          name: args.name,
+          age: args.age,
+        });
+        return author.save();
+      },
+    },
+  },
+});
+
 module.exports = new graphql.GraphQLSchema({
   query: RootQuery,
+  mutation: Mutation,
 });
