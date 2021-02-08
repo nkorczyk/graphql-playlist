a6d2177f0d754c58dc8ef1e0096f044229a978e1
Norbert Korczyk <norbert_korczyk@epam.com>
2021-02-08 11:25:13 +0100

    Apollo client setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,21 @@
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+
 import BookList from "./components/BookList";
+
+// apollo client setup
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql",
+});
 
 const App = () => {
   return (
-    <div id="main">
-      <h1>Ninja's Reading List</h1>
-      <BookList />
-    </div>
+    <ApolloProvider client={client}>
+      <div id="main">
+        <h1>Ninja's Reading List</h1>
+        <BookList />
+      </div>
+    </ApolloProvider>
   );
 };
 
